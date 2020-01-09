90c9a208608710b650a0b2f34877a6693c5594cc
unknown <delfdevs@gmail.com>
2020-01-08 23:29:37 -0500

    msgform

--- src/components/CreateJokeForm.js
+++ src/components/CreateJokeForm.js
@@ -1,28 +1,82 @@
 import React, { useState } from "react";
 
-const CreateJokeForm = () => {
+const CreateJokeForm = ({ addNewJoke }) => {
+  // console.log("this is our props",props);
+  const [joke, setJoke] = useState({ id: "", title: "", body: "" });
+
+  const handleChanges = event => {
+    setJoke({ ...joke, [event.target.name]: event.target.value });
+  };
+
+  const submitForm = event => {
+    event.preventDefault();
+    addNewJoke(joke);
+    setJoke({ id: "", title: "", body: "" });
+  };
+
   return (
-    <form>
-      <label htmlFor="title"> Tell me a joke.</label>
+    <form onSubmit={submitForm}>
+      <label htmlFor="title">Tell me a joke.</label>
+      <br />
+      <br />
       <input
         id="title"
         type="text"
         name="title"
         placeholder="Enter a joke"
-      //   onChange={handleChanges}
-      //   value={note.title}
+        onChange={handleChanges}
+        value={joke.title}
       />
-      <label htmlFor="note"> Dads Joke</label>
+      <br />
+      <br />
+      <label htmlFor="joke"> Dads Joke </label>
+      <br />
+      <br />
       <textarea
-        id="note"
+        id="joke"
         name="body"
-        placeholder="Add Joke here"
-      //   onChange={handleChanges}
-      //   value={note.body}
+        placeholder=" Add Joke here "
+        onChange={handleChanges}
+        value={joke.body}
       />
-      <button type="submit"> Add Note</button>
+      <br />
+      <br />
+      <button type="submit"> Add Jokes </button>
     </form>
   );
-}
+};
+
+
+
+
+// import React, { useState } from "react";
+
+// const CreateJokeForm = () => {
+        
+ 
+//         return (
+//             <form>
+//               <label htmlFor="title"> Tell me a joke.</label>
+//               <input 
+//               id="title" 
+//               type="text" 
+//               name="title" 
+//               placeholder="Enter a joke" 
+//             //   onChange={handleChanges}
+//             //   value={note.title}
+//               />
+//               <label htmlFor="note"> Dads Joke</label>
+//               <textarea 
+//               id="note" 
+//               name="body" 
+//               placeholder="Add Joke here"
+//             //   onChange={handleChanges}
+//             //   value={note.body}
+//               />
+//               <button type="submit"> Add Jokes</button>
+//             </form>
+//             );
+
+// }
 
 export default CreateJokeForm;
