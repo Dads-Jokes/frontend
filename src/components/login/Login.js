70227c8cc7c86639d03854734e2514a106b717a0
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2020-01-08 19:52:11 -0600

    deleted something

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Login = (props) => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+
   const [loginData, setLoginData] = useState({
     username: "",
     password: ""
